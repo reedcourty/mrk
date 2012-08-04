aedc82d385f9e3a688086c727f2723c257de25eb
Gyorgy Nadudvari <reedcourty@gmail.com>
2012-08-04 12:07:05 +0200

    Az mplayer stdout-ját nézegetjük címre vadászva, és ha találunk, akkor kiírjuk.

---
+++ title_reader.py
@@ -0,0 +1,8 @@
+import subprocess
+
+process = subprocess.Popen(['d:/System/Programs/MPlayer/NoGUI/mplayer.exe', 'http://mr-stream.mediaconnect.hu/4738/mr2.mp3'], shell=False, stdout=subprocess.PIPE)
+
+for line in process.stdout:
+    if (line.find("ICY Info: StreamTitle=") != -1):
+        if (line.find("Nagyon Zene!") == -1):
+            print(line.split("'")[1])
