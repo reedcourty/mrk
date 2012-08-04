369f99248935a9797a73ee5340962da957c2e663
Gyorgy Nadudvari <reedcourty@gmail.com>
2012-08-04 12:17:15 +0200

    Az aktuális szám címét kiírjuk egy fájlba.

--- title_reader.py
+++ title_reader.py
@@ -1,5 +1,11 @@
 import subprocess
 
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+print(PROJECT_PATH)
+
 process = subprocess.Popen(['d:/System/Programs/MPlayer/NoGUI/mplayer.exe',
                             '-vo', 'null', '-ao', 'null',
                             'http://mr-stream.mediaconnect.hu/4738/mr2.mp3'],
@@ -8,4 +14,8 @@ process = subprocess.Popen(['d:/System/Programs/MPlayer/NoGUI/mplayer.exe',
 for line in process.stdout:
     if (line.find("ICY Info: StreamTitle=") != -1):
         if (line.find("Nagyon Zene!") == -1):
-            print(line.split("'")[1])
+            title = line.split("'")[1]
+            print(title)
+            f = open(PROJECT_PATH + "/nowplaying", "w")
+            f.write(title)
+            f.close()
